cb328f4fd55b3e4dfed641c3bffc822f6dcd2a3c
Emil Widlund <hello@emilwidlund.com>
2025-01-03 17:57:43 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# polar-nextjs
+
+Payments and Checkouts made dead simple with Next.js.
+
+## Checkout
+
+Create a Checkout handler which takes care of redirections.
+
+## Webhooks
+
+A simple utility which resolves incoming webhook payloads by signing the webhook secret properly.
---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import { Polar } from "@polar-sh/sdk";
+import { type NextRequest, NextResponse } from "next/server";
+import {
+	validateEvent,
+	WebhookVerificationError,
+} from "@polar-sh/sdk/webhooks";
+
+export interface CheckoutConfig {
+	accessToken?: string;
+	successUrl?: string;
+	includeCheckoutId?: boolean;
+}
+
+export const Checkout = ({
+	accessToken,
+	successUrl,
+	includeCheckoutId = true,
+}: CheckoutConfig) => {
+	const polar = new Polar({
+		accessToken,
+	});
+
+	return async (req: NextRequest) => {
+		const url = new URL(req.url);
+		const productId = url.searchParams.get("productId");
+
+		if (!productId) {
+			return NextResponse.json(
+				{ error: "Missing productId in query params" },
+				{ status: 400 },
+			);
+		}
+
+		const success = successUrl ? new URL(successUrl) : undefined;
+
+		if (success && includeCheckoutId) {
+			success.searchParams.set("checkoutId", "{CHECKOUT_ID}");
+		}
+
+		try {
+			const result = await polar.checkouts.custom.create({
+				productId,
+				successUrl,
+				customerId: url.searchParams.get("customerId") ?? undefined,
+				customerEmail: url.searchParams.get("customerEmail") ?? undefined,
+				customerName: url.searchParams.get("customerName") ?? undefined,
+				customerBillingAddress: url.searchParams.has("customerBillingAddress")
+					? JSON.parse(url.searchParams.get("customerBillingAddress") ?? "{}")
+					: undefined,
+				customerTaxId: url.searchParams.get("customerTaxId") ?? undefined,
+				customerIpAddress:
+					url.searchParams.get("customerIpAddress") ?? undefined,
+				customerMetadata: url.searchParams.has("customerMetadata")
+					? JSON.parse(url.searchParams.get("customerMetadata") ?? "{}")
+					: undefined,
+				allowDiscountCodes: url.searchParams.has("allowDiscountCodes")
+					? url.searchParams.get("allowDiscountCodes") === "true"
+					: undefined,
+				discountId: url.searchParams.get("discountId") ?? undefined,
+				metadata: url.searchParams.has("metadata")
+					? JSON.parse(url.searchParams.get("metadata") ?? "{}")
+					: undefined,
+			});
+
+			return NextResponse.redirect(result.url);
+		} catch (error) {
+			console.error(error);
+			return NextResponse.error();
+		}
+	};
+};
+
+export interface WebhooksConfig {
+	webhookSecret: string;
+	onPayload: (payload: ReturnType<typeof validateEvent>) => Promise<void>;
+}
+
+export const Webhooks = ({ webhookSecret, onPayload }: WebhooksConfig) => {
+	return async (request: NextRequest) => {
+		const requestBody = await request.text();
+
+		const webhookHeaders = {
+			"webhook-id": request.headers.get("webhook-id") ?? "",
+			"webhook-timestamp": request.headers.get("webhook-timestamp") ?? "",
+			"webhook-signature": request.headers.get("webhook-signature") ?? "",
+		};
+
+		let webhookPayload: ReturnType<typeof validateEvent>;
+		try {
+			webhookPayload = validateEvent(
+				requestBody,
+				webhookHeaders,
+				webhookSecret,
+			);
+		} catch (error) {
+			if (error instanceof WebhookVerificationError) {
+				return NextResponse.json({ received: false }, { status: 403 });
+			}
+
+			throw error;
+		}
+
+		await onPayload(webhookPayload);
+
+		return NextResponse.json({ received: true });
+	};
+};
