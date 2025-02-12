8bcad804b08cfdee4b8ec07352921c088794ea90
Emil Widlund <hello@emilwidlund.com>
2025-02-12 15:36:31 +0100

    use textencoder in fetch start

--- packages/ingestion/src/strategies/FetchStrategy/FetchStrategy.ts
+++ packages/ingestion/src/strategies/FetchStrategy/FetchStrategy.ts
@@ -1,57 +1,67 @@
 import type { Polar } from "@polar-sh/sdk";
 import {
-  IngestionStrategy,
-  type IngestionExecutionHandler,
+	IngestionStrategy,
+	type IngestionExecutionHandler,
 } from "../../strategy";
 import type { IngestionContext } from "../../ingestion";
 
 type Fetch = typeof fetch;
 
 const wrapFetch = (
-  httpClient: Fetch,
-  execute: IngestionExecutionHandler<FetchStrategyContext>,
-  customerId: string,
+	httpClient: Fetch,
+	execute: IngestionExecutionHandler<FetchStrategyContext>,
+	customerId: string,
 ): Fetch => {
-  return async (input, init) => {
-    const response = await httpClient(input, init);
+	return async (input, init) => {
+		const response = await httpClient(input, init);
 
-    const url =
-      typeof input === "string"
-        ? input
-        : "url" in input
-          ? input.url
-          : input.toString();
+		const url =
+			typeof input === "string"
+				? input
+				: "url" in input
+					? input.url
+					: input.toString();
 
-    execute({
-      url,
-      method: init?.method ?? "GET",
-      customerId,
-    });
+		const method = init?.method ?? "GET";
 
-    return response;
-  };
+		const encoder = new TextEncoder();
+		const bytes = encoder.encode(init?.body?.toString() ?? "").length;
+
+		execute({
+			url,
+			method,
+			bytes,
+			customerId,
+		});
+
+		return response;
+	};
 };
 
 type FetchStrategyContext = IngestionContext<{
-  url: string;
-  method: string;
+	// Request URL
+	url: string;
+	// Request method
+	method: string;
+	// Bytes sent in the request
+	bytes: number;
 }>;
 
 export class FetchStrategy extends IngestionStrategy<
-  FetchStrategyContext,
-  Fetch
+	FetchStrategyContext,
+	Fetch
 > {
-  private fetchClient: Fetch;
+	private fetchClient: Fetch;
 
-  constructor(fetchClient: Fetch, polar: Polar) {
-    super(polar);
+	constructor(fetchClient: Fetch, polar: Polar) {
+		super(polar);
 
-    this.fetchClient = fetchClient;
-  }
+		this.fetchClient = fetchClient;
+	}
 
-  override client(customerId: string): Fetch {
-    const executionHandler = this.createExecutionHandler();
+	override client(customerId: string): Fetch {
+		const executionHandler = this.createExecutionHandler();
 
-    return wrapFetch(this.fetchClient, executionHandler, customerId);
-  }
+		return wrapFetch(this.fetchClient, executionHandler, customerId);
+	}
 }
--- packages/ingestion/src/strategies/S3Strategy/S3Strategy.ts
+++ packages/ingestion/src/strategies/S3Strategy/S3Strategy.ts
@@ -2,64 +2,87 @@ import type { S3Client } from "@aws-sdk/client-s3";
 import type { Polar } from "@polar-sh/sdk";
 import type { IngestionContext } from "../../ingestion";
 import {
-  type IngestionExecutionHandler,
-  IngestionStrategy,
+	type IngestionExecutionHandler,
+	IngestionStrategy,
 } from "../../strategy";
 
 type S3StrategyContext = IngestionContext & {
-  bucket: string;
-  key: string;
-  bytes: number;
+	bucket?: string;
+	key?: string;
+	contentType?: string;
+	bytes: number;
 };
 
 export class S3Strategy extends IngestionStrategy<S3StrategyContext, S3Client> {
-  private s3Client: S3Client;
+	private s3Client: S3Client;
 
-  constructor(s3Client: S3Client, polar: Polar) {
-    super(polar);
+	constructor(s3Client: S3Client, polar: Polar) {
+		super(polar);
 
-    this.s3Client = s3Client;
-  }
+		this.s3Client = s3Client;
+	}
 
-  private wrapS3Client({
-    s3Client,
-    execute: _execute,
-    customerId: _customerId,
-  }: {
-    s3Client: S3Client;
-    execute: IngestionExecutionHandler<S3StrategyContext>;
-    customerId: string;
-  }) {
-    const plugin: Parameters<S3Client["middlewareStack"]["use"]>[0] = {
-      applyToStack: (stack) => {
-        stack.add(
-          (next, context) => async (args) => {
-            const result = await next(args);
+	private wrapS3Client({
+		s3Client,
+		execute,
+		customerId,
+	}: {
+		s3Client: S3Client;
+		execute: IngestionExecutionHandler<S3StrategyContext>;
+		customerId: string;
+	}) {
+		const plugin: Parameters<S3Client["middlewareStack"]["use"]>[0] = {
+			applyToStack: (stack) => {
+				stack.add(
+					(next, _context) => async (args) => {
+						const result = await next(args);
 
-            console.log({ args, context, result });
+						if ("request" in args) {
+							const payload: S3StrategyContext = {
+								bytes: Number.parseInt(
+									// @ts-expect-error
+									args.request.headers["content-length"] ?? "0",
+								),
+								customerId,
+							};
 
-            return result;
-          },
-          {
-            step: "deserialize",
-            priority: "high",
-          },
-        );
-      },
-    };
+							if ("Bucket" in args.input) {
+								payload.bucket = args.input.Bucket;
+							}
 
-    s3Client.middlewareStack.use(plugin);
+							if ("Key" in args.input) {
+								payload.key = args.input.Key;
+							}
 
-    return s3Client;
-  }
+							if ("ContentType" in args.input) {
+								payload.contentType = args.input.ContentType;
+							}
 
-  public client(customerId: string): S3Client {
-    const execute = this.createExecutionHandler();
+							execute(payload);
+						}
 
-    return this.wrapS3Client({
-      s3Client: this.s3Client,
-      execute,
-      customerId,
-    });
-  }
+						return result;
+					},
+					{
+						step: "deserialize",
+						priority: "high",
+					},
+				);
+			},
+		};
+
+		s3Client.middlewareStack.use(plugin);
+
+		return s3Client;
+	}
+
+	public client(customerId: string): S3Client {
+		const execute = this.createExecutionHandler();
+
+		return this.wrapS3Client({
+			s3Client: this.s3Client,
+			execute,
+			customerId,
+		});
+	}
 }
