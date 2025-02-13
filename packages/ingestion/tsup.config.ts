bb77018afd341eefd52f3a965ee938e8cfde7313
Emil Widlund <hello@emilwidlund.com>
2025-02-13 14:36:07 +0100

    remove ingestion lib from adapters

--- packages/ingestion/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  outDir: "dist",
-});
